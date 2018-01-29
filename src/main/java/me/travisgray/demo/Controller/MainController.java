2c3ad0f175aa9152946c77a52dca7c64a0a2a1f4
Travis-Gray2194 <travisogray1@gmail.com>
2018-01-29 15:57:04 -0500

    1.Created Repository for Resume 2.Created Controller with default route.
    3rd Commit

--- src/main/java/me/travisgray/demo/Controller/MainController.java
+++ src/main/java/me/travisgray/demo/Controller/MainController.java
@@ -1,10 +1,46 @@
 package me.travisgray.demo.Controller;
 
+import me.travisgray.demo.Models.Resume;
+import me.travisgray.demo.Repositories.EducationRepository;
+import me.travisgray.demo.Repositories.ExperinceRepository;
+import me.travisgray.demo.Repositories.ResumeRepository;
+import me.travisgray.demo.Repositories.SkillsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    SkillsRepository skillsRepository;
+
+    @Autowired
+    EducationRepository educationRepository;
+
+    @Autowired
+    ExperinceRepository experinceRepository;
+
+    @Autowired
+    ResumeRepository resumeRepository;
+
+
+    @RequestMapping("/")
+    public String showIndex(Model model){
+        model.addAttribute("getskills", skillsRepository.count());
+        model.addAttribute("skilllist",skillsRepository.findAll());
+        model.addAttribute("geteducation",educationRepository.count());
+        model.addAttribute("educationlist",educationRepository.findAll());
+        model.addAttribute("getexperince",experinceRepository.count());
+        model.addAttribute("experincelist",experinceRepository.findAll());
+        model.addAttribute("resume",resumeRepository.count());
+        model.addAttribute("resumelist",resumeRepository.findAll());
+        return "index";
+
+    }
+
 
     
 
---
+++ src/main/java/me/travisgray/demo/Repositories/ResumeRepository.java
@@ -0,0 +1,7 @@
+package me.travisgray.demo.Repositories;
+
+import me.travisgray.demo.Models.Resume;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ResumeRepository extends CrudRepository<Resume,Long> {
+}
